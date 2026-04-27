b954689fec1c18b0b55871224d2007b3bc00bec2
hojin476 <hojin7070@mju.ac.kr>
2026-04-27 11:27:41 +0000

    main.c 수정2

--- main.c
+++ main.c
@@ -11,11 +11,13 @@ int main()
 //1 변수 선언 (자료형 3개 이상, 변수 5개 이상)
 char user_name;
 int goal_weight;
+int choice;
 double benchpress;
 double squat;
 double body_weight;
 double total_volume;
 double gap;
+double ratio;
 
 printf("============================================\n");
 printf("\t오운완(오늘 운동 완료) 기록기\n");
@@ -37,6 +39,48 @@ scanf("%lf", &benchpress);
 printf("스쿼트 무게(kg)를 입력하세요 : ");
 scanf("%lf", &squat);
 
+printf("\n============================================\n");
+
+//3.1 산술 연산
+total_volume = benchpress + squat;
+gap = goal_weight - total_volume;
+ratio = total_volume / body_weight;
+
+//3.2 조건문
+if (benchpress > 0 && squat > 0)
+{
+  //다중 조건문
+  if (ratio >= 2.0)
+  {
+    printf("등급: 헬창 (대단합니다!)\n");
+
+  }
+  else if(ratio >= 1.0)
+  {
+    printf("등급: 득근자 (열심히 하시네요!)\n");
+  }
+  else
+  {
+    printf("등급: 헬린이 (열심히 합시다!)\n");
+  }
+
+  // 목표 달성 여부 확인
+  if (total_volume >= goal_weight)
+  {
+    printf("목표 중량도 달성했습니다!\n");
+  }
+  else
+  {
+    printf("아쉽습니다 목표 중량까지 %lfkg 남았습니다\n", gap);
+  }
+
+  // [switch-case 문] 메뉴 선택 기능
+  printf("\n추가 확인 메뉴 (1: 상세 기록, 2: 조언 보기, 3: 종료) : ");
+  scanf("%d", &choice);
+  
+}
+
+/*
 //3 산술 연산
 total_volume = benchpress + squat;
 gap = goal_weight - total_volume;
@@ -50,6 +94,7 @@ printf("오늘 들어올린 총 무게: %.1f\n", total_volume);
 printf("목표 달성까지 %.1fkg 남았습니다!\n", gap);
 printf("============================================\n");
 printf("\t  오운완! 수고하셨습니다!\n");
+*/
 
   return 0;
 }
